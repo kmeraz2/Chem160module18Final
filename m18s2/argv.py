dd798ba3aec16083fad1aa7906f80413bcf4d78d
kmeraz2 <kmeraz2@ucmerced.edu>
2019-12-17 23:15:54 -0800

    first commit

---
+++ m18s2/argv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+print("I see ", len(sys.argv), " arguments")
+print("They are: ",sys.argv))
+if ys.argv[1].isnumeric():
+	print("First arg is numeric: ",int(sys.argv[1]))
+else:
+	print("First arg os non-numeric")
